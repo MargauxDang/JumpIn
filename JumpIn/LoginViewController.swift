05fb572612033a685031464caff83df4eb164507
MargauxDang <margaux.dang@esigetel.net>
2017-11-01 10:02:53 +0100

    Login and sign up

--- JumpIn/LoginViewController.swift
+++ JumpIn/LoginViewController.swift
@@ -1,24 +1,21 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  JumpIn
 //
-//  Created by Margaux Dang on 17/10/2017.
+//  Created by Margaux Dang on 01/11/2017.
 //  Copyright © 2017 Margaux Dang. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
-    @IBOutlet var start: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        start.layer.cornerRadius = 5.0
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
